be1c779daadf2256b66c592c419d9dd98bace0dc
BehindOfDarkness <135264191+BehindOfDarkness@users.noreply.github.com>
2023-06-08 19:04:51 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React, { useState } from 'react';
+
+// export default App;
+function App() {
+
+	const [users, setUsers] = useState([])
+
+	const getData = () => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(res => res.json())
+			.then(res => {
+				if (res && Array.isArray && res.length > 0) {
+					setUsers(res)
+
+				}
+			})
+	}
+
+	const loadUsers = () => {
+		getData()
+	}
+	const styles = {
+		color: 'black',
+		border: '1px solid black',
+		padding: '10px',
+		margin: '0 auto',
+		borderRaduis: '5px',
+		backgroundColor: 'aquamarine'
+	}
+	return (
+		<>
+			<h2> Users: <button type='' onClick={() => { loadUsers() }}>Загрузить список пользователей</button> </h2>
+			<div style={{ margin: 50 }}>
+				{users.length > 0 && users.map(user => {
+					return <p key={Math.random()} style={styles}>Имя:  {user.name},   Никнейм : {user.username},   email : {user.email}</p>
+
+
+				})
+				}
+			</div>
+		</>
+	)
+}
+
+export default App;
